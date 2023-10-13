7814a675db1a3728042e010ad679c34d48c1a72e
CMScofield7 <llbchagas@gmail.com>
2023-10-13 17:17:08 -0300

    Criando GET para pegar todos os usuários

--- src/main/java/cm/scofield/todolist/user/UserController.java
+++ src/main/java/cm/scofield/todolist/user/UserController.java
@@ -3,6 +3,7 @@ package cm.scofield.todolist.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,11 @@ public class UserController {
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping()
+    public ResponseEntity get() {
+        var users = this.userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 }
